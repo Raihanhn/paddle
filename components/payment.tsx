febc7243581df93f72ebdeafd1aa33ebf90dcccb
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2026-02-22 13:05:17 +0600

    payment component added to home page

--- components/payment.tsx
+++ components/payment.tsx
@@ -1,19 +1,37 @@
-"use client"
+"use client";
 
-import { initializePaddle, Paddle } from '@paddle/paddle-js'
-import React, { useEffect, useState } from 'react'
+import { initializePaddle, Paddle } from "@paddle/paddle-js";
+import React, { useEffect, useState } from "react";
 
-export default function payment() {
-    const [paddle, setPaddle] = useState<Paddle>();
+export default function Payment() {
+  const [paddle, setPaddle] = useState<Paddle>();
 
-    useEffect(() => {
-        initializePaddle({
-          environment: 'sandbox',
-          token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN!,
-        });
-    }, [])
+  useEffect(() => {
+    initializePaddle({
+      environment: "sandbox",
+      token: process.env.NEXT_PUBLIC_PADDLE_CLIENT_TOKEN!,
+    }).then((paddle) => setPaddle(paddle));
+  }, []);
+
+  const handleCheckout = () => {
+    if (!paddle) return alert("Paddle not intialized");
+
+    paddle.Checkout.open({
+      items: [{ priceId: "pro_01kj1yrgfxq56w7xm715bq2nyp", quantity: 1 }],
+      settings: {
+        displayMode: "overlay",
+        theme: "dark",
+        successUrl: "http:localhost:3000/success",
+      },
+    });
+  };
 
   return (
-    <div>payment</div>
-  )
+    <button
+      className="bg-blue-500 text-white px-4 py-2 rounded-md"
+      onClick={handleCheckout}
+    >
+      Proceed to Payment
+    </button>
+  );
 }
